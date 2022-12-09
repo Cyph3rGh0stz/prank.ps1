6d0dd7bfd7c1698d49d4ad4daac1158aaf53c568
anon8105 <114602720+anon8105@users.noreply.github.com>
2022-12-09 08:53:13 -0700

    Add files via upload

---
+++ automation.ps1
@@ -0,0 +1,10 @@
+echo "Your computer has been hacked."
+sleep 1
+echo "Your files have been encrypted by Zephyr."
+sleep 1
+echo "If you would like to have your files restored please contact this email." 
+echo "zephyrresponseteam@mail.com"
+sleep 10
+echo "I can't believe you fell for that. This is a prank powershell script that says you have been hacked."
+sleep 1
+echo "You need to look more closely at what you download."
